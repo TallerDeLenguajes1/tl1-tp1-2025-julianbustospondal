1aa6fb1b807534749afb3ed170def74cbb281e59
julian <julianbustospondal@gmail.com>
2026-04-06 16:50:39 -0300

    4.f

--- tp1_1.c
+++ tp1_1.c
@@ -2,7 +2,20 @@
 
 int main()
 {
-    printf("hola mundo");
+    printf("hola mundo \n");
+
+    int mi_variable = 25;
+    int *puntero = &mi_variable;
+
+    printf("1) El contenido (valor) al que apunta el puntero: %d\n", *puntero);
+
+    printf("2) La direccion almacenada por el puntero: %p\n", puntero);
+
+    printf("3) La direccion de memoria de la variable: %p\n", &mi_variable);
+
+    printf("4) La direccion de memoria del propio puntero: %p\n", &puntero);
+
+    printf("5) El tamano de memoria de la variable es: %zu bytes\n", sizeof(mi_variable));
 
     return 0;
 }
